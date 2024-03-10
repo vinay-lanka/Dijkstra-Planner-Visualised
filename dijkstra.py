c99615a5d5b7f9e425a046950a4de346c4521c56
Vinay Lanka <vinaylanka111@gmail.com>
2024-03-09 20:19:23 -0500

    add support for input and input validation

--- dijkstra.py
+++ dijkstra.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
- 
+
 #Initialising variables
 start_pos = [0,0]
 goal_pos = [0,0]
@@ -46,8 +46,59 @@ def generate_map():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def input_validation(position):
+    #Check if input in map
+    if 5 < position[0] < 1195:
+        pass
+    else:
+        return False
+    
+    if 5 < map_size[1] - position[1] < 495:
+        pass
+    else:
+        return False
+    
+    if np.all(frame[position[1],position[0]]):
+        return True
+    else:
+        return False
+
+def set_start_goal_pos():
+    global start_pos, goal_pos
+    positions_valid = False
+    print("Enter Start and Goal Coordinates")
+    print("Map size is 1200x500 with a clearance of 5")
+    print("Enter Coordinate as x,y (x and y seperated with a comma)")
+    while not positions_valid:
+        start_ip = input("Enter start x,y coordinate: ") or "6,6"
+        start_pos = list(int(x) for x in start_ip.split(","))
+        start_pos[1] = map_size[1] - start_pos[1]
+        positions_valid = input_validation(start_pos)
+        if not positions_valid:
+            print("Enter valid coordinates")
+            continue
+        goal_ip = input("Enter goal x,y coordinate: ") or "1194,494"
+        goal_pos = list(int(x) for x in goal_ip.split(","))
+        goal_pos[1] = map_size[1] - goal_pos[1]
+        positions_valid = input_validation(goal_pos)
+        if not positions_valid:
+            print("Enter valid coordinates")
+            continue
+    
+    start_goal_frame = frame
+    start_goal_frame = cv2.circle(start_goal_frame, start_pos, 5, (0,0,255), -1) 
+    start_goal_frame = cv2.circle(start_goal_frame, goal_pos, 5, (255,0,0), -1) 
+    cv2.imshow("Starting positons", start_goal_frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+ 
+
 def main():
     generate_map()
+    set_start_goal_pos()
+    print("Starting position of the robot", start_pos[0], ",", map_size[1] - start_pos[1])
+    print("Goal position of the robot", goal_pos[0], ",", map_size[1] - goal_pos[1])
 
 if __name__ == "__main__":
     main()
